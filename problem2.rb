f541233b0e3940a691c9b4c8946e495d97fe667b
sarahincapie <saral85@gmail.com>
2016-02-10 11:25:20 -0500

    solved conflicts

--- problem2.rb
+++ problem2.rb
@@ -8,12 +8,12 @@ trio = answer.capitalize
 
 puts "STOP DOING THAT #{trio}"
 
-<<<<<<< HEAD
+
 
 def 
 	2 + 2
 	
 end
-=======
+
 This is probably gonna break.
->>>>>>> b2c6a02ba1f5107ee516af9eae8e4ae82c5b9d57
+
